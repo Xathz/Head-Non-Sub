ca6142167c253eb45f2b44a36047287a01691324
Xathz <xathz@users.noreply.github.com>
2019-05-07 19:55:19 -0400

    feat: MEE6 infractions now translates channel id's to names
    Add deleteallinfractions command.

--- Head Non-Sub/Clients/Discord/Commands/Exclamation/MeeSix.cs
+++ Head Non-Sub/Clients/Discord/Commands/Exclamation/MeeSix.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 using Discord.Commands;
 using Discord.WebSocket;
 using HeadNonSub.Clients.Discord.Attributes;
+using HeadNonSub.Entities.Discord.MessageTag;
 using HeadNonSub.Entities.MeeSix.Moderator;
 using HeadNonSub.Extensions;
 using HeadNonSub.Settings;
@@ -23,26 +25,40 @@ namespace HeadNonSub.Clients.Discord.Commands.Exclamation {
         public async Task Infractions(SocketUser user = null) {
             if (user == null) { return; }
 
+            await Context.Channel.TriggerTypingAsync();
+
             Moderator result = GetUserInfractions(Context.Guild.Id, user.Id);
 
             if (result is Moderator) {
                 StringBuilder builder = new StringBuilder();
 
                 foreach (Infraction infraction in result.Infractions) {
+
+                    List<MessageTag> tags = infraction.Reason.ParseDiscordMessage();
+                    string reason = infraction.Reason;
+
+                    foreach (MessageTag tag in tags.Where(x => x.TagType == TagType.Channel)) {
+                        SocketGuildChannel channel = Context.Guild.GetChannel(tag.Id);
+
+                        if (channel is SocketGuildChannel) {
+                            reason = reason.Replace(tag.ToString(), $"#{channel.Name}");
+                        }
+                    }
+
                     if (ulong.TryParse(infraction.AuthorId, out ulong authorId)) {
                         SocketUser authorUser = UserFromUserId(authorId);
-                        builder.AppendLine($"{infraction.CreatedAtDateTime.ToString(Constants.DateTimeFormatMedium).ToLower()} utc ({infraction.Id}) {(authorUser == null ? "Unknown Moderator" : authorUser.ToString())} ● {infraction.Reason}");
+                        builder.AppendLine($"{infraction.CreatedAtDateTime.ToString(Constants.DateTimeFormatMedium).ToLower()} utc ({infraction.Id}) {(authorUser == null ? "Unknown Moderator" : authorUser.ToString())} ● {reason}");
                     } else {
-                        builder.AppendLine($"{infraction.CreatedAtDateTime.ToString(Constants.DateTimeFormatMedium).ToLower()} utc ({infraction.Id}) {infraction.Reason}");
+                        builder.AppendLine($"{infraction.CreatedAtDateTime.ToString(Constants.DateTimeFormatMedium).ToLower()} utc ({infraction.Id}) Unknown Moderator ● {reason}");
                     }
                 }
 
-                List<string> chunks = builder.ToString().SplitIntoChunksPreserveNewLines(1950);
+                List<string> chunks = builder.ToString().SplitIntoChunksPreserveNewLines(1930);
 
                 await Task.Delay(500);
 
                 foreach (string chunk in chunks) {
-                    await BetterReplyAsync($"```{chunk}```", user.Id.ToString());
+                    await BetterReplyAsync($"● Infractions for {BetterUserFormat(user)} ```{chunk}```", user.Id.ToString());
                 }
             } else {
                 await BetterReplyAsync($"Failed to retrieve infractions for {BetterUserFormat(user)}.");
@@ -64,6 +80,29 @@ namespace HeadNonSub.Clients.Discord.Commands.Exclamation {
             } else {
                 await BetterReplyAsync("The infraction was **not** deleted.", id);
             }
+        }
+
+        [Command("deleteallinfractions")]
+        [DiscordStaffOnly]
+        public async Task DeleteAllInfractions(SocketUser user = null) {
+            if (user == null) {
+                await BetterReplyAsync("You must mention a user to clear all their infractions.");
+                return;
+            }
+
+            await Context.Channel.TriggerTypingAsync();
+
+            Moderator result = GetUserInfractions(Context.Guild.Id, user.Id);
+
+            int deleteCount = 0;
+            foreach (Infraction infraction in result.Infractions) {
+
+                if (DeleteInfraction(Context.Guild.Id, infraction.Id)) {
+                    deleteCount++;
+                }
+            }
+
+            await BetterReplyAsync($"● **{deleteCount}** infractions for {BetterUserFormat(user)} were deleted.", user.Id.ToString());
         }
 
         private Moderator GetUserInfractions(ulong serverId, ulong userId) {
